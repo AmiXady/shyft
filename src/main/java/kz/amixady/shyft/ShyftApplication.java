1f171998f9a4539f8cd376a59db49a3d260d872f
LetMeDiie <1-1_2m@mail.ru>
2025-07-28 18:16:49 +0500

    Добавлена структура проекта

---
+++ src/main/java/kz/amixady/shyft/ShyftApplication.java
@@ -0,0 +1,11 @@
+package kz.amixady.shyft;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyftApplication {
+	public static void main(String[] args) {
+        System.out.println("Sex");
+	}
+
+}
---
+++ src/test/java/kz/amixady/shyft/ShyftApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.amixady.shyft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShyftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
